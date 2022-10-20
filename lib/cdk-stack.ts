352720b446a98908d4bac78c57b6956692cdea63
Logan Murphy <nagolyhprum@hotmail.com>
2022-10-20 18:59:05 -0400

    update scripts

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -5,8 +5,9 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import * as codedeploy from 'aws-cdk-lib/aws-codedeploy';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
-import { LaunchTemplate } from 'aws-cdk-lib/aws-ec2';
+
+const REGION = "us-east-1"
+const BUCKET_NAME = `aws-codedeploy-${REGION}`;
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -48,21 +49,56 @@ export class CdkStack extends cdk.Stack {
       },      
     })
 
-    const NextJSVpc = new ec2.Vpc(this, "NextJSVpc")
+    const NextJSVpc = new ec2.Vpc(this, "NextJSVpc", {})
 
-    const NextJSAutoScalingGroup = new autoscaling.AutoScalingGroup(this, "NextJSAutoScalingGroup", {
-      vpc : NextJSVpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.NANO),
-      machineImage: new ec2.AmazonLinuxImage(),
+    const NextJSSecurityGroup = new ec2.SecurityGroup(this, "NextJSSecurityGroup", {
+      vpc : NextJSVpc,            
+      allowAllOutbound : true,      
     })
+    
+    NextJSSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.allTraffic(), "In")
+    // NextJSSecurityGroup.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.allTraffic(), "Out")
 
-    NextJSBucket.grantReadWrite(NextJSAutoScalingGroup)
+    const NextJS = new ec2.Instance(this, "NextJS", {
+      vpc : NextJSVpc,
+      instanceType: new ec2.InstanceType("t2.nano"),
+      machineImage: new ec2.AmazonLinuxImage(),
+      securityGroup : NextJSSecurityGroup,      
+      keyName : "NextJS",      
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },      
+      init: ec2.CloudFormationInit.fromConfigSets({
+        configSets: {
+          default: ['yumPreinstall'],
+        },
+        configs: {
+          yumPreinstall: new ec2.InitConfig([
+            ec2.InitPackage.yum('ruby'),
+            ec2.InitPackage.yum('wget'),
+            ec2.InitCommand.argvCommand(["cd", "/home/ec2-user"]),
+            // https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+            ec2.InitCommand.argvCommand(["wget", `https://${BUCKET_NAME}.s3.${REGION}.amazonaws.com/latest/install`]),
+            ec2.InitCommand.argvCommand(["chmod", "+x", "./install"]),
+            ec2.InitCommand.argvCommand(["sudo", "./install", "auto"]),
+          ]),
+        },
+      }),
+    })
+    cdk.Tags.of(NextJS).add('version', '1.0.0');
+    NextJSBucket.grantReadWrite(NextJS)
+
+    const NextJSEIP = new ec2.CfnEIP(this, "NextJSEIP", {
+      instanceId : NextJS.instanceId,            
+    })
 
     const NextJSApplication = new codedeploy.ServerApplication(this, "NextJSApplication", {})
 
     const NextJSDeploymentGroup = new codedeploy.ServerDeploymentGroup(this, "NextJSDeploymentGroup", {
       application : NextJSApplication,   
-      autoScalingGroups : [NextJSAutoScalingGroup],
+      ec2InstanceTags : new codedeploy.InstanceTagSet({
+        "version" : ["1.0.0"]
+      }),
     })
   }
 }
