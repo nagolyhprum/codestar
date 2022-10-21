53efaedc250b17dca23719d95ee6c931bebc5826
Logan Murphy <nagolyhprum@hotmail.com>
2022-10-21 12:05:19 -0400

    allow all traffic

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -55,11 +55,9 @@ export class CdkStack extends cdk.Stack {
 
     const NextJSSecurityGroup = new ec2.SecurityGroup(this, "NextJSSecurityGroup", {
       vpc : NextJSVpc,    
-      allowAllOutbound : false        
     })
     
-    NextJSSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), "HTTP In")
-    NextJSSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), "SSH In")
+    NextJSSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.allTraffic(), "In")
 
     const NextJSAutoScalingGroup = new autoscaling.AutoScalingGroup(this, "NextJSAutoScalingGroup", {
       vpc : NextJSVpc,
